aee169ba1b4aadbd9219b9d51cc6a9e1608817b0
MalikTufail <88918989+MalikTufail@users.noreply.github.com>
2022-03-10 21:03:30 +0500

    2nd commit

--- cypress/integration/plevryLogin.spec.js
+++ cypress/integration/plevryLogin.spec.js
@@ -37,22 +37,33 @@ describe('Login functionality', () => {
         login.signIn(signIn)
         login.validationMessages()
     })
-    it('verify the validation message in netherland if user provide the incorrect email', () => {
-        cy.visit('/login')
-        const {invalidEmail, password} = plevry
-        login.emailPlevery(invalidEmail)
-        login.passwordPlevery(password)
-        const {signIn} = selectors.login
-        login.signIn(signIn)
-        login.validationMessages()
-    })
     it('verify the validation message when the user left the email and password field blank', () => {
         cy.visit('/en/login')
         const {signIn} = selectors.login
         login.signIn(signIn)
         login.validationMessages()
-        cy.contains("Email address is required").should('be.visible')
     })
+    it('verify the validation message if user typed the password in capital letters', () => {
+        cy.visit('en/login')
+        const {email, passwordCapital} = plevry
+        login.emailPlevery(email)
+        login.passwordPlevery(passwordCapital)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    })
+    it('Verify if show/hide icon is functional', () =>{
+        cy.visit('en/login')
+        const {email, password} = plevry
+        login.emailPlevery(email)
+        login.passwordPlevery(password)
+        login.showHideIcon()
+    })
+    it('Verify the functionality of language change button', () => {
+        cy.visit('/en/login')
+        login.languageChange()
+    })
+    
     it('verify if the user can be log in with capital email and valid password', () => {
         cy.visit('/en/login')
         const {emailCapital, password} = plevry
@@ -70,5 +81,85 @@ describe('Login functionality', () => {
         const {signIn} = selectors.login
         login.signIn(signIn)
         cy.contains(/points of sale/i).should('be.visible')
+    })
+    it('verify the validation message in "Dutch" if user provide the incorrect email', () => {
+        cy.visit('/login')
+        const {invalidEmail, password} = plevry
+        login.emailPlevery(invalidEmail)
+        login.passwordPlevery(password)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    })
+    it('Verify if the user cannot be login with the invalid credentials "Dutch"', () => {
+        cy.visit('/login')
+        const {invalidEmail, invalidPassword} = plevry
+        login.emailPlevery(invalidEmail)
+        login.passwordPlevery(invalidPassword)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    })
+    it('Verify if the user cannot be login with valid email and invalid password "Dutch"', () => {
+        cy.visit('/login')
+        const {email, invalidPassword} = plevry
+        login.emailPlevery(email)
+        login.passwordPlevery(invalidPassword)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    });
+    it('verify if the user cannot be login with invalid email and valid password "Dutch"', () => {
+        cy.visit('/login')
+        const {invalidEmail, password} = plevry
+        login.emailPlevery(invalidEmail)
+        login.passwordPlevery(password)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    })
+    it('verify the validation message when the user left the email and password field blank "Dutch"', () => {
+        cy.visit('/login')
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    })
+    it('verify the validation message if user typed the password in capital letters "Dutch"', () => {
+        cy.visit('/login')
+        const {email, passwordCapital} = plevry
+        login.emailPlevery(email)
+        login.passwordPlevery(passwordCapital)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        login.validationMessages()
+    })
+    it('Verify if show/hide icon is functional "Dutch"', () =>{
+        cy.visit('/login')
+        const {email, password} = plevry
+        login.emailPlevery(email)
+        login.passwordPlevery(password)
+        login.showHideIcon()
+    })
+    it('verify if the user can be log in with capital email and valid password "Dutch"', () => {
+        cy.visit('/login')
+        const {emailCapital, password} = plevry
+        login.emailPlevery(emailCapital)
+        login.passwordPlevery(password)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        cy.contains(/Verkooppunten/i).should('be.visible')
+    })
+    it('verify if user can be login with valid email and valid password "Dutch"', () => {
+        cy.visit('/login')
+        const {email, password} = plevry
+        login.emailPlevery(email)
+        login.passwordPlevery(password)
+        const {signIn} = selectors.login
+        login.signIn(signIn)
+        cy.contains(/Verkooppunten/i).should('be.visible')
+    })
+    it('Verify the functionality of language change button "Dutch"', () => {
+        cy.visit('/login')
+        login.languageChange()
     })
 })
--- cypress/pageObject/plevryLogin.js
+++ cypress/pageObject/plevryLogin.js
@@ -13,33 +13,62 @@ class Login {
     signIn (signIn) {
         cy.xpath(signIn).then($signinButton => {
             let signinButton = $signinButton.text()
-            // const {nthSignIn} = plevry
             if(signinButton === "Inloggen") {
                 cy.get($signinButton).click()
             } else {
                 cy.get($signinButton).click()
             }
         })
-        
     }
     validationMessages() {
         const {validationLogin} = selectors.login
         cy.xpath(validationLogin).then( $validationMsgs => {
             let validationMsgs = $validationMsgs[0].innerText
-            const {emailAndPassword, emailRequired, passwordRequired, nthEmailAndPassword} = plevry
+            const {emailAndPassword, emailRequired, passwordRequired, nthEmailAndPassword, passCapitalVal} = plevry
             if(validationMsgs === emailAndPassword){
                 cy.contains($validationMsgs[0].innerText).should('be.visible')
                 cy.contains($validationMsgs[0].innerText).should('contain', emailAndPassword)
             }
-            else if(validationMsgs === emailRequired  && validationMsgs === passwordRequired) {
+            else if(validationMsgs === emailRequired) {
                 cy.contains($validationMsgs[0].innerText).should('be.visible')
-                cy.contains($validationMsgs[0].innerText).should('contain', emailRequired)
-                cy.contains($validationMsgs[0].innerText).should('contain', passwordRequired)
+                cy.xpath('(//p)[2]').should('be.visible')
+            }
+            else if (validationMsgs === passCapitalVal) {
+                cy.contains($validationMsgs[0].innerText).should('be.visible')
             }
             else if (validationMsgs === nthEmailAndPassword) {
                 cy.contains($validationMsgs[0].innerText).should('be.visible')
                 cy.contains($validationMsgs[0].innerText).should('contain', nthEmailAndPassword)
             }
+            else if(validationMsgs === "E-mailadres is verplicht") {
+                cy.contains($validationMsgs[0].innerText).should('be.visible')
+                cy.xpath('(//p)[2]').should('be.visible')
+            }
+            else if (validationMsgs === "Gebruik minimaal één hoofdletter, één kleine letter, één cijfer en één speciaal karakter") {
+                cy.contains($validationMsgs[0].innerText).should('be.visible')
+                
+            }
+        })
+    }
+    showHideIcon() {
+        const {userPassword, showHideIcon} = selectors.login
+        cy.get(userPassword).should('have.attr', 'type', 'password')
+        cy.xpath(showHideIcon).click()
+        cy.get(userPassword).should('have.attr', "type", 'text')
+    }
+    languageChange() {
+        cy.xpath('/*//label').then ( $emailLabel => {
+        let emailLabel = $emailLabel[0].innerText
+        if(emailLabel === 'Email address'){
+            cy.contains($emailLabel[0].innerText).should('be.visible')
+            cy.get('select[id="language-selector"]').select('nl')
+            cy.contains(/E-mailadres/i).should('be.visible')
+        }
+        else {
+            cy.contains($emailLabel[0].innerText).should('be.visible')
+            cy.get('select[id="language-selector"]').select('en')
+            cy.contains(/Email address/i).should('be.visible')
+        }
         })
     }
 }
